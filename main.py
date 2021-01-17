2f151c6664423284dc4fde69c38edcca60bdb0ea
DimonONE <68736542+DimonONE@users.noreply.github.com>
2021-01-17 13:41:35 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+from For_admin import Product
+from CREATE_BASE import ADD_PRODUCT
+from telebot import types
+import telebot
+import time
+import config
+from mwt import MWT
+
+bot = telebot.TeleBot(config.TOKEN)
+
+
+@MWT(timeout=60*60)
+def get_admin_ids(bot, chat_id):
+    return [admin.user.id for admin in bot.get_chat_administrators(chat_id)]
+
+
+@bot.message_handler(commands=['create'])
+def start_message(message):
+    if message.from_user.id in get_admin_ids(bot, message.chat.id):
+        bot.send_message(
+            message.chat.id, text=" * Создание нового товара: * ", reply_markup=Product.add())
+    else:
+        bot.send_message(
+            message.chat.id, text=" * Ви не админ!!!: * ")
+
+
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
